69ee0ba501147c99e04feaeda2cd70da29aa8885
akhilsharma6 <akhilsharma69@yahoo.co.in>
2016-11-13 23:39:45 +0530

    Assignment 3.1

---
+++ Test.java
@@ -0,0 +1,15 @@
+public class Test {
+	static{
+		print(10);
+	}
+	
+	static void print(int x) {
+		System.out.println (x);
+		System.exit(0);
+	}
+}
+
+/* This program when run will return an error during run-time.
+   Although the most apt answer to available option is b. Will throw a NoSuchMethod error at runtime.
+   but this is not true. It throws error: Main method not found in class Test.
+*/
